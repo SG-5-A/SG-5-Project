c4659bcb92ff70e04a1c281d9a91962156eb5ebd
Arttu111 <n2kuar00@students.oamk.fi>
2022-12-14 10:44:33 +0200

    Maantieto js ja css

--- JavaScript/maantieto.js
+++ JavaScript/maantieto.js
@@ -1,1 +1,23 @@
 
+
+let accordion1 = document.getElementById("accordionExample");
+let answers = document.getElementById("next");
+let start = document.getElementById("startButton");
+answers.addEventListener("click", openaccordion1)
+
+
+/**
+ * @param {Event} event
+ */
+ 
+
+function openaccordion1(event){
+
+    let clickedbutton = event.currentTarget;
+    answerClicked = clickedbutton;
+    accordion1.classList.add("visible");
+    start.classList.add("remove");
+   
+
+}
+
