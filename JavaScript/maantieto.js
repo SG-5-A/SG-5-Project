4ca2f3e69126038edf9e57e0f4f29aff5a5ea861
Arttu111 <n2kuar00@students.oamk.fi>
2022-12-14 11:16:03 +0200

    maantieto added js and buttons for results

--- JavaScript/maantieto.js
+++ JavaScript/maantieto.js
@@ -3,8 +3,12 @@
 let accordion1 = document.getElementById("accordionExample");
 let answers = document.getElementById("next");
 let start = document.getElementById("startButton");
+let resultButton = document.getElementById("resultButton");
+let resultB = document.getElementById("resultB");
+let results = document.getElementById("resultDiv");
+let infoText = document.getElementById("textContainer");
 answers.addEventListener("click", openaccordion1)
-
+resultB.addEventListener("click", openResults)
 
 /**
  * @param {Event} event
@@ -13,11 +17,22 @@ answers.addEventListener("click", openaccordion1)
 
 function openaccordion1(event){
 
-    let clickedbutton = event.currentTarget;
-    answerClicked = clickedbutton;
+    //let clickedbutton = event.currentTarget;
+    //answerClicked = clickedbutton;
     accordion1.classList.add("visible");
     start.classList.add("remove");
-   
+    resultButton.classList.add("visible");
+    infoText.classList.add("visible");
 
 }
+/**
+ * @param {event} event
+ */
+function openResults(event){
+    accordion1.classList.remove("visible");
+    resultButton.classList.remove("visible");
+    results.classList.add("visible");
+    infoText.classList.remove("visible");
 
+
+}
